179a7968afba20bc8a71c1d96b4590ba40d72bd2
saksh19 <153769302+saksh19@users.noreply.github.com>
2025-09-05 03:11:23 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ export default function Home() {
           <p>software developer fresher</p>
           <p>{profile.email}</p>
 
-          <div className="flex gap-4">
+          <div className="flex gap-4" style={{ gap:16px}}>
             <a href={profile.github} target="_blank" rel="noreferrer">
               <button className="btn btn-primary">GitHub</button>
             </a>
