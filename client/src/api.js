36b9c825d86f17ae057c0e31f69a5fcac92e2fb3
saksh19 <153769302+saksh19@users.noreply.github.com>
2025-09-05 00:51:08 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000'  // backend base
+  baseURL: 'me-api-playground-production-cb54.up.railway.app'  // backend base
 });
 
 // attach token automatically
